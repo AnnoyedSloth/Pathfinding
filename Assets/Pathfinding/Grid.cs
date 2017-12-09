3ce60494fa15011d1ce120a707ac328a40704c63
DESKTOP-IKDVREU\박수환 <sksk31052@hanmail.net>
2017-12-09 20:34:35 +0900

    BFS Finding algorithm applied

--- Assets/Pathfinding/Grid.cs
+++ Assets/Pathfinding/Grid.cs
@@ -9,10 +9,11 @@ public class Grid : MonoBehaviour
 {
 
     public GameObject player;
-    private List<int> visited;
-    [SerializeField] private Queue<int> neighborsQueue;
+    [SerializeField] private List<int> visited;
+    private Queue<int> neighborsQueue;
     [SerializeField] private int curLocation;
     int dequeueNode;
+    private int?[] parentNode;
     System.Diagnostics.Stopwatch timer;
 
     public Text destText;
@@ -20,9 +21,11 @@ public class Grid : MonoBehaviour
 
     const int CELL_COUNT = 30;
 
-    bool[,] walls = new bool[CELL_COUNT, CELL_COUNT];
-    private bool[,] isBlocked = new bool[CELL_COUNT, CELL_COUNT];
+    [SerializeField] bool[] walls = new bool[CELL_COUNT * CELL_COUNT];
+    private bool[] isBlocked = new bool[CELL_COUNT * CELL_COUNT];
     [SerializeField] private bool[] isVisited = new bool[CELL_COUNT * CELL_COUNT];
+    [SerializeField] private bool[] isChecked = new bool[CELL_COUNT * CELL_COUNT];
+    [SerializeField] private List<int> path;
 
     // Use this for initialization
     void Start()
@@ -30,6 +33,13 @@ public class Grid : MonoBehaviour
         timer = new System.Diagnostics.Stopwatch();
         visited = new List<int>();
         neighborsQueue = new Queue<int>();
+        parentNode = new int?[CELL_COUNT * CELL_COUNT];
+        path = new List<int>();
+
+        for (int a = 0; a < CELL_COUNT * CELL_COUNT; a++)
+        {
+            parentNode[a] = null;
+        }
 
         for (int a = 0; a < CELL_COUNT; a++)
         {
@@ -37,24 +47,36 @@ public class Grid : MonoBehaviour
             Debug.DrawLine(new Vector3(0, 0, a), new Vector3(CELL_COUNT, 0, a), Color.green, Mathf.Infinity);
         }
 
-        for (int a = 0; a < CELL_COUNT; a++)
+        for (int a = 0; a < CELL_COUNT * CELL_COUNT; a++)
         {
-            for (int b = 0; b < CELL_COUNT; b++)
-            {
-                walls[a, b] = false;
-                isBlocked[a, b] = false;
-                isVisited[a * 30 + b] = false;
-            }
+
+            walls[a] = false;
+            isBlocked[a] = false;
+            isVisited[a] = false;
+            isChecked[a] = false;
         }
 
-        for (int a = 0; a < CELL_COUNT; a++)
+        for (int a = 0; a < CELL_COUNT * CELL_COUNT; a++)
         {
-            for (int b = 0; b < CELL_COUNT; b++)
-            {
-                if (Random.Range(0, 10) > 7) walls[a, b] = true;
-            }
+
+                if (Random.Range(0, 10) > 5) walls[a] = true;
+            
         }
-        //WallGenerate(walls);
+        WallGenerate(walls);
+    }
+
+    private void Init()
+    {
+        for (int a = 0; a < CELL_COUNT * CELL_COUNT; a++)
+        {
+            isBlocked[a] = false;
+            isVisited[a] = false;
+            isChecked[a] = false;
+        }
+        for (int a = 0; a < CELL_COUNT * CELL_COUNT; a++)
+        {
+            parentNode[a] = null;
+        }
     }
 
     // Update is called once per frame
@@ -77,13 +99,14 @@ public class Grid : MonoBehaviour
                 if (hit.point.y > player.transform.localPosition.y + 0.3f)
                 {
                     //print(hit.point.y);
-                    print("You're blocked by wall mate.");
+                    print(Vector3ToCellNum(loc) + "You're blocked by wall mate.");
                 }
                 else
                 {
                     print("New Start");
                     //print(Vector3ToCellNum(loc));
                     //to prevent infinite loop
+                    Init();
                     FindDest(loc);
                 }
             }
@@ -99,6 +122,7 @@ public class Grid : MonoBehaviour
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 cube.transform.position = new Vector3(loc.x + 0.5f, loc.y + 0.5f, loc.z + 0.5f);
                 cube.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                walls[Vector3ToCellNum(loc)] = true;
             }
         }
     }
@@ -116,50 +140,32 @@ public class Grid : MonoBehaviour
 
         for (int a = 0; a < NeighborCell(curLocation).Count; a++)
         {
-            print("Neighbor" + a + " : " + NeighborCell(curLocation)[a]);
+            //print("Neighbor" + a + " : " + NeighborCell(curLocation)[a]);
             neighborsQueue.Enqueue(NeighborCell(curLocation)[a]);
+            isChecked[NeighborCell(curLocation)[a]] = true;
         }
 
-        for (int a = 0; a < 10000; a++)
+        isChecked[curLocation] = true;
+        isVisited[curLocation] = true;
+
+        if(BFSSearch(dest) == null)StartCoroutine(BFSSearch(dest));
+        else
         {
-            dequeueNode = neighborsQueue.Dequeue();
-
-            if (dequeueNode == Vector3ToCellNum(dest)) { print("Found!"); break; }
-
-            print("DequeueNode : " + dequeueNode);
-            for (int b = 0; b < NeighborCell(dequeueNode).Count; b++)
-            {
-                //print("Neighbor" + b + " : " + NeighborCell(dequeueNode)[b]);
-                if (!neighborsQueue.Contains(dequeueNode))
-                {
-                    neighborsQueue.Enqueue(NeighborCell(dequeueNode)[b]);
-                }
-                Debug.DrawLine(new Vector3(dequeueNode % CELL_COUNT, 0.1f, dequeueNode / CELL_COUNT), new Vector3(dequeueNode % CELL_COUNT + 1, 0.0f, dequeueNode / CELL_COUNT + 1), Color.blue, Mathf.Infinity);
-            }
-
-            if (!isVisited[dequeueNode])
-            {
-                visited.Add(dequeueNode);
-                isVisited[dequeueNode] = true;
-                //Debug.DrawLine(new Vector3(dequeueNode % CELL_COUNT, 0.2f, dequeueNode / CELL_COUNT + 1), new Vector3(dequeueNode % CELL_COUNT + 1, 0.0f, dequeueNode / CELL_COUNT), Color.red, Mathf.Infinity);
-
-            }
+            StopCoroutine(BFSSearch(dest));
+            StartCoroutine(BFSSearch(dest));
         }
 
     }
 
-    void WallGenerate(bool[,] _walls)
+    void WallGenerate(bool[] _walls)
     {
-        for (int a = 0; a < CELL_COUNT; a++)
+        for (int a = 0; a < CELL_COUNT * CELL_COUNT; a++)
         {
-            for (int b = 0; b < CELL_COUNT; b++)
+            if (_walls[a] == true && a != curLocation)
             {
-                if (_walls[a, b] == true && Vector3ToCellNum(new Vector3(a, 0, b)) != curLocation)
-                {
-                    GameObject newcube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    newcube.transform.position = new Vector3(a + 0.5f, 0.5f, b + 0.5f);
-                    //newcube.AddComponent<Material>()
-                }
+                GameObject newcube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                newcube.transform.position = CellNumToVector3(a) + new Vector3(0, 0.5f, 0);
+                //newcube.AddComponent<Material>()
             }
         }
     }
@@ -167,7 +173,7 @@ public class Grid : MonoBehaviour
     List<int> NeighborCell(int cellNum)
     {
         List<int> neighbors = new List<int>();
-        string[] str = new string[3];
+        //string[] str = new string[3];
 
         for (int a = 0; a < 3; a++)
         {
@@ -177,7 +183,21 @@ public class Grid : MonoBehaviour
                 if (a == 1 && b != 1) neighbors.Add(cellNum - 1 + b);
                 if (a == 2 && (cellNum / CELL_COUNT) != 0) neighbors.Add(cellNum - CELL_COUNT - 1 + b);
 
-                str[a] = str[a] + neighbors[b].ToString() + ", ";
+                if (cellNum % CELL_COUNT == 0)
+                {
+                    neighbors.Remove(cellNum - 1);
+                    neighbors.Remove(cellNum + CELL_COUNT - 1);
+                    neighbors.Remove(cellNum - CELL_COUNT - 1);
+                }
+
+                if (cellNum % CELL_COUNT == CELL_COUNT - 1)
+                {
+                    neighbors.Remove(cellNum - 1);
+                    neighbors.Remove(cellNum + CELL_COUNT + 1);
+                    neighbors.Remove(cellNum - CELL_COUNT + 1);
+                }
+
+                //str[a] = str[a] + neighbors[b].ToString() + ", ";
             }
         }
         return neighbors;
@@ -198,4 +218,71 @@ public class Grid : MonoBehaviour
         return new Vector3(cellNum % CELL_COUNT, 0.1f, cellNum / CELL_COUNT + 0.5f);
     }
 
+    void DrawPath(int childNode)
+    {
+        int beforeNode = childNode;
+
+        while (beforeNode != Vector3ToCellNum(player.transform.position))
+        {
+            LineDraw(beforeNode, Color.red, 1);
+            path.Add(beforeNode);
+            beforeNode = (int)parentNode[beforeNode];
+        }
+
+        //if (childNode == Vector3ToCellNum(player.transform.position)) return childNode;
+        //else
+        //{
+
+        //    return DrawPath((int)parentNode[childNode]);
+        //}
+    }
+
+    void LineDraw(int cellNum, Color color, int mode)
+    {
+        if(mode==0)Debug.DrawLine(new Vector3(cellNum % CELL_COUNT, 0.1f, cellNum / CELL_COUNT), new Vector3(cellNum % CELL_COUNT + 1, 0.0f, cellNum / CELL_COUNT + 1), color, Mathf.Infinity);
+        else if(mode==1)Debug.DrawLine(new Vector3(cellNum % CELL_COUNT, 0.1f, cellNum / CELL_COUNT+1), new Vector3(cellNum % CELL_COUNT+1, 0.0f, cellNum / CELL_COUNT), color, Mathf.Infinity);
+    }
+
+    IEnumerator BFSSearch(Vector3 destVector)
+    {
+        while (true)
+        {
+            if (neighborsQueue.Count > 0)
+            {
+                dequeueNode = neighborsQueue.Dequeue();
+
+                if (dequeueNode == Vector3ToCellNum(destVector))
+                {
+                    print("Found! : " + Vector3ToCellNum(destVector));
+                    DrawPath(dequeueNode);
+                    StopCoroutine(BFSSearch(destVector));
+                    break;
+                }
+
+                print("DequeueNode : " + dequeueNode);
+                if (!isVisited[dequeueNode] && !walls[dequeueNode])
+                {
+                    for (int b = 0; b < NeighborCell(dequeueNode).Count; b++)
+                    {
+                        //print("Neighbor" + b + " : " + NeighborCell(dequeueNode)[b]);
+                        if (!isChecked[NeighborCell(dequeueNode)[b]] && !walls[NeighborCell(dequeueNode)[b]])
+                        {
+                            neighborsQueue.Enqueue(NeighborCell(dequeueNode)[b]);
+                            print("Enqueued Data : " + NeighborCell(dequeueNode)[b]);
+                            parentNode[NeighborCell(dequeueNode)[b]] = dequeueNode;
+                            print("CellNo : " + NeighborCell(dequeueNode)[b] + ", ParentNo : " + parentNode[NeighborCell(dequeueNode)[b]]);
+                            isChecked[NeighborCell(dequeueNode)[b]] = true;
+                            //LineDraw(dequeueNode, Color.red, 1);
+                        }
+                        //Debug.DrawLine(new Vector3(dequeueNode % CELL_COUNT, 0.2f, dequeueNode / CELL_COUNT + 1), new Vector3(dequeueNode % CELL_COUNT + 1, 0.0f, dequeueNode / CELL_COUNT), Color.red, Mathf.Infinity);
+                    }
+                    visited.Add(dequeueNode);
+                    isVisited[dequeueNode] = true;
+                    LineDraw(dequeueNode, Color.blue, 0);
+                    //Debug.DrawLine(new Vector3(dequeueNode % CELL_COUNT, 0.1f, dequeueNode / CELL_COUNT), new Vector3(dequeueNode % CELL_COUNT + 1, 0.0f, dequeueNode / CELL_COUNT + 1), Color.blue, Mathf.Infinity);
+                }
+            }
+            yield return null;//new WaitForSeconds(0.01f);
+        }
+    }
 }
